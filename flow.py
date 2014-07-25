6ad6042f401f4332debf3fe1b7e29424d71ac9ed
jkeifer <jarrettkeifer@gmail.com>
2014-07-24 17:25:53 -0700

    Fixes and enhancements

--- flow.py
+++ flow.py
@@ -18,11 +18,11 @@ def extract_capture_metadata(cap):
     extracts metadata on framerate, resolution, codec, and length from opencv video capture object
     '''
     cv_fourcc_code = cap.get(6)
-    frame_rate = cap.get(5)
-    frame_height = cap.get(4)
-    frame_width = cap.get(3)
-    video_length = timedelta(seconds=(cap.get(7) * (1 / frame_rate)))
-    return (cv_fourcc_code, frame_rate, frame_height, frame_width, video_length)
+    FRAME_RATE = cap.get(5)
+    FRAME_HEIGHT = cap.get(4)
+    FRAME_WIDTH = cap.get(3)
+    VIDEO_LENGTH = timedelta(seconds=(cap.get(7) * (1 / FRAME_RATE)))
+    return (cv_fourcc_code, FRAME_RATE, FRAME_HEIGHT, FRAME_WIDTH, VIDEO_LENGTH)
 
 def setup_display_windows():
     '''
@@ -38,40 +38,32 @@ def setup_display_windows():
     cv2.moveWindow(PLOT_WINDOW, 0, 400)
 
     #resize window
-    cv2.resizeWindow(VIDEO_WINDOW, 500, int(frame_width / 500 * frame_height))
-    cv2.resizeWindow(VECTOR_WINDOW, 500, int(frame_width / 500 * frame_height))
-    cv2.resizeWindow(PLOT_WINDOW, 500, int(frame_width / 500 * frame_height))
+    cv2.resizeWindow(VIDEO_WINDOW, 500, int(FRAME_WIDTH / 500 * FRAME_HEIGHT))
+    cv2.resizeWindow(VECTOR_WINDOW, 500, int(FRAME_WIDTH / 500 * FRAME_HEIGHT))
+    cv2.resizeWindow(PLOT_WINDOW, 500, int(FRAME_WIDTH / 500 * FRAME_HEIGHT))
 
-def draw_text_overlay(vectorField, varianceBuffer, movementBuffer, variance, movement, histogram, newFeatures=False):
+def draw_text_overlay(vectorField,
+                      varianceLRmean, varianceUDmean, movementLRmean, movementUDmean,
+                      varianceLR, movementLR, varianceUD, movementUD):
     '''
     draws text overlay on vector field window
     '''
 
-    if len(varianceBuffer) > BUFFER_LENGTH:
-        varianceMean = np.nanmean(varianceBuffer[-BUFFER_LENGTH:])
-        movementMean = np.nanmean(movementBuffer[-BUFFER_LENGTH:])
-        #if variance > 1 and varianceMean > 1:
-        if varianceMean > THRESHOLD:
-            cv2.putText(vectorField, 'navigating', (0,60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,225,0))
-        elif varianceMean < THRESHOLD and abs(movementMean) < LOOKING_THRESHOLD:
-            cv2.putText(vectorField, 'stopped', (0,60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0))
-        elif varianceMean < THRESHOLD and abs(movementMean) > LOOKING_THRESHOLD:
-            cv2.putText(vectorField, 'looking', (0,60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0))
-        else:
-            cv2.putText(vectorField, 'thinking...', (0,60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,255))
-    else:
-        if variance > 1:
-            cv2.putText(vectorField, 'navigating', (0,60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,225,0))
-        else:
-            cv2.putText(vectorField, 'stopped', (0,60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0))
+    cv2.putText(vectorField, 'variance L-R: %s, %s' % (varianceLR, varianceLRmean), (0,20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0))
+    cv2.putText(vectorField, 'movement L-R: %s, %s' % (movementLR, movementLRmean), (0,40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0))
+    cv2.putText(vectorField, 'variance U-D: %s, %s' % (varianceUD, varianceUDmean), (0,60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0))
+    cv2.putText(vectorField, 'movement U-D: %s, %s' % (movementUD, movementUDmean), (0,80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0))
 
-    cv2.putText(vectorField, 'magnitude variance: %s' % variance, (0,20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0))
-
-    if newFeatures is False:
-        cv2.putText(vectorField, 'movement: %s' % movement, (0,40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0))
+    if varianceLRmean > HIGH_VARIANCE_LR or varianceUDmean > HIGH_VARIANCE_UD:
+        cv2.putText(vectorField, 'navigating', (0,100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,225,0))
+    elif (varianceLRmean < MEDIUM_VARIANCE_LR or varianceUDmean < MEDIUM_VARIANCE_UD) and (movementLRmean > MEDIUM_MOVING_LR or movementUDmean > MEDIUM_MOVING_UD):
+        if movementUDmean < LOW_MOVING_UD:
+            cv2.putText(vectorField, 'rotating', (0,100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,225,0))
+        else:
+            cv2.putText(vectorField, 'looking', (0,100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,225,0))
     else:
-        movement = 0
-        cv2.putText(vectorField, 'movement: Recalculating...', (0,40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0))
+        cv2.putText(vectorField, 'stopped', (0,100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,225,0))
+    cv2.waitKey(200)
 
 def update_windows(frame, vectorField, plot):
     # display to screen
@@ -85,7 +77,7 @@ def find_features(framequeue, feature_params):
     while (features is None or len(features) <= MIN_FEATURES):
         old_frame = framequeue[-iterations]
         old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-        features = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+        features = cv2.goodFeaturesToTrack(old_gray, mask=None, **feature_params)
         iterations += 1
         if iterations > len(framequeue):
             raise NoFeatures("Could not find sufficient features in last {0} frames.".format(iterations - 1))
@@ -100,27 +92,42 @@ def recalculate_new_features(framequeue, feature_params, lk_params, frame_gray, 
     #when redrawing throw away old features
     #p1 = p0
     #newFeatures = True
-    if not p1 is None:
+    if not p1 is None and len(p1) > MIN_FEATURES:
         good_new = p1[st==1]
+        good_old = p0[st==1]
     else:
         good_new = []
-    good_old = p0[st==1]
+        good_old = []
+        p0 = None
+        p1 = None
+        raise NoFeatures("Did not detect enough features in current frame.")
     return (p0, p1, good_new, good_old)
 
 
 # statics
+# CROPPING
+LINES_FROM_TOP = 50
+LINES_FROM_BOTTOM = 0
+
 # length of buffer for smoothing
-BUFFER_LENGTH = 30
+BUFFER_LENGTH = 10
 FRAME_QUEUE_LENGTH = 10
 
-# threshold for navigation vs stopping
-THRESHOLD = 0.8
-LOOKING_THRESHOLD = 2
+# thresholds for navigation vs stopping
+# variance thresholds
+LOW_VARIANCE_THRESHOLD    = 1
+MEDIUM_VARIANCE_THRESHOLD = 5
+HIGH_VARIANCE_THRESHOLD   = 10
+
+# movement thresholds
+LOW_MOVING_THRESHOLD    = 1
+MEDIUM_MOVING_THRESHOLD = 5
+HIGH_MOVING_THRESHOLD   = 10
 
 # number of corners to detect
 MAX_FEATURES = 100
-MIN_FEATURES = 10
-RECALC_PERCENTAGE = 0.25
+MIN_FEATURES = 20
+RECALC_PERCENTAGE = 0.6
 
 # option to display images to screen
 DISPLAY = True
@@ -130,7 +137,8 @@ VIDEO_WINDOW = 'img'
 VECTOR_WINDOW = 'vectorField'
 PLOT_WINDOW = 'plot'
 
-invideofile = "./IMG_0066.MOV"
+invideofile = "./IMG_0067.MOV"
+#invideofile = "./subj8.avi"
 outdir = "./"
 
 filename, ext = os.path.splitext(os.path.basename(invideofile))
@@ -138,14 +146,36 @@ outvideofile = os.path.join(os.path.dirname(invideofile), filename + "_processed
 
 cap = cv2.VideoCapture(invideofile)
 
-cv_fourcc_code, frame_rate, frame_height, frame_width, video_length = extract_capture_metadata(cap)
+cv_fourcc_code, FRAME_RATE, FRAME_HEIGHT, FRAME_WIDTH, VIDEO_LENGTH = extract_capture_metadata(cap)
+
+# make thresholds relative to frame dimensions
+LOW_VARIANCE_LR    = LOW_VARIANCE_THRESHOLD / FRAME_WIDTH
+MEDIUM_VARIANCE_LR = MEDIUM_VARIANCE_THRESHOLD / FRAME_WIDTH
+HIGH_VARIANCE_LR   = HIGH_VARIANCE_THRESHOLD / FRAME_WIDTH
+
+LOW_VARIANCE_UD    = LOW_VARIANCE_THRESHOLD / FRAME_HEIGHT
+MEDIUM_VARIANCE_UD = MEDIUM_VARIANCE_THRESHOLD / FRAME_HEIGHT
+HIGH_VARIANCE_UD   = HIGH_VARIANCE_THRESHOLD / FRAME_HEIGHT
+
+LOW_MOVING_LR    = LOW_MOVING_THRESHOLD / FRAME_WIDTH
+MEDIUM_MOVING_LR = MEDIUM_MOVING_THRESHOLD / FRAME_WIDTH
+HIGH_MOVING_LR   = HIGH_MOVING_THRESHOLD / FRAME_WIDTH
+
+LOW_MOVING_UD    = LOW_MOVING_THRESHOLD / FRAME_HEIGHT
+MEDIUM_MOVING_UD = MEDIUM_MOVING_THRESHOLD / FRAME_HEIGHT
+HIGH_MOVING_UD   = HIGH_MOVING_THRESHOLD / FRAME_HEIGHT
+
+print("LR Variance Low: {0}, Med: {1}, High: {2}".format(LOW_VARIANCE_LR, MEDIUM_VARIANCE_LR, HIGH_VARIANCE_LR))
+print("UD Variance Low: {0}, Med: {1}, High: {2}".format(LOW_VARIANCE_UD, MEDIUM_VARIANCE_UD, HIGH_VARIANCE_UD))
+print("LR Moving   Low: {0}, Med: {1}, High: {2}".format(LOW_MOVING_LR, MEDIUM_MOVING_LR, HIGH_MOVING_LR))
+print("UD Moving   Low: {0}, Med: {1}, High: {2}".format(LOW_MOVING_UD, MEDIUM_MOVING_UD, HIGH_MOVING_UD))
 
 #mp4v works pretty well on wide variety of installs of opencv and ffmpeg
 #mpeg-4 video
 fourcc = cv2.cv.CV_FOURCC(*'mp4v')
 
-print(fourcc, cv_fourcc_code, frame_height, frame_width, frame_rate)
-writer = cv2.VideoWriter(outvideofile, fourcc, frame_rate, (int(frame_width), int(frame_height)), True)
+print(fourcc, cv_fourcc_code, FRAME_HEIGHT, FRAME_WIDTH, FRAME_RATE)
+writer = cv2.VideoWriter(outvideofile, fourcc, FRAME_RATE, (int(FRAME_WIDTH), int(FRAME_HEIGHT)), True)
 
 # try is to allow finally statement at end to always close files properly
 try:
@@ -166,12 +196,22 @@ try:
 
     # Take first frame and find corners in it
     ret, old_frame = cap.read()
+    old_frame = old_frame[LINES_FROM_TOP:, 25:-24]
+    #cropmask = cv2.CreateImage(cv2.GetSize(old_frame), cv2.CV_8UC1, 1)
+    #cropmask[:LINES_FROM_TOP] = 0
+
+    cropmask = np.copy(old_frame)
+    cropmask[:LINES_FROM_TOP] = 0
+    cropmask[LINES_FROM_TOP:] = 1
+
+    # CROP FRAME
+    #old_frame = old_frame[LINES_FROM_TOP:-LINES_FROM_BOTTOM]
 
     framequeue = deque(maxlen=FRAME_QUEUE_LENGTH)
     framequeue.append(old_frame)
 
     old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-    p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+    p0 = cv2.goodFeaturesToTrack(old_gray, mask=None, **feature_params)
 
     # Create a mask image for drawing purposes
     mask = np.zeros_like(old_frame)
@@ -184,15 +224,18 @@ try:
 
     #setup variance and movement buffer
     #contains data extracted from previous frames
-    varianceBuffer = []
-    movementBuffer = []
+    varianceLRBuffer = deque(maxlen=BUFFER_LENGTH)
+    movementLRBuffer = deque(maxlen=BUFFER_LENGTH)
+    varianceUDBuffer = deque(maxlen=BUFFER_LENGTH)
+    movementUDBuffer = deque(maxlen=BUFFER_LENGTH)
 
     #for rendering plot
     plot = np.zeros_like(old_frame)
 
     while(1):
+        frame_count = cap.get(1)
         timestamp = timedelta(seconds=(cap.get(0) / 1000))
-        sys.stdout.write("Processed {0} of {1}\r".format(timestamp, video_length))
+        sys.stdout.write("Processed {0} of {1}\r".format(timestamp, VIDEO_LENGTH))
         sys.stdout.flush()
         newFeatures = False
 
@@ -209,15 +252,27 @@ try:
 
         ret, frame = cap.read()
 
+        # CROP FRAME
+        #frame = frame[LINES_FROM_TOP:-LINES_FROM_BOTTOM]
+
         if ret==True:
             try:
                 iterations = 1
+                frame = frame[LINES_FROM_TOP:, 25:-24]
                 frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
                 if p0 is None:
-                    p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
-                    varianceBuffer.append(np.nan)
-                    movementBuffer.append(np.nan)
+                    p0 = cv2.goodFeaturesToTrack(frame_gray, mask=None, **feature_params)
+                    varianceLRBuffer.append(np.nan)
+                    movementLRBuffer.append(np.nan)
+                    varianceUDBuffer.append(np.nan)
+                    movementUDBuffer.append(np.nan)
+                    old_gray = frame_gray.copy()
+                    old_frame = frame.copy()
+                    framequeue.append(old_frame)
+                    cv2.putText(vectorField, 'recalculating', (0,100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,225,0))
+                    writer.write(vectorField)
+                    update_windows(frame, vectorField, plot)
                     continue
                 else:
                     # calculate optical flow
@@ -229,40 +284,30 @@ try:
                 else:
                     good_new = []
 
-
-                # check if features should be recalculated
-                #if len(good_new) < MIN_FEATURES or len(good_new) < (RECALC_PERCENTAGE * len(p1)):
-
-                # if len(good_new) < MIN_FEATURES:
-                #     p0, p1, good_new, good_old = recalculate_new_features(framequeue, feature_params, lk_params, frame_gray, p0, p1, good_new, good_old)
-                #     #old_frame = frame.copy()
-                #     #cv2.imshow(VIDEO_WINDOW,frame)
-                #     #cv2.waitKey(25)
-                #     #skip to next frame
-                #     print 'recreated features... skipping to next frame - %s' % dt.datetime.now()
-                #
-                #     #continue
-
-                if len(good_new) < MIN_FEATURES:
+                if len(good_new) < MIN_FEATURES or frame_count % 10 == 0:
                     try:
                         old_gray, p0, iterations = find_features(framequeue, feature_params)
                         print("Found {0} features in {1} iteration(s)".format(len(p0), iterations + 1))
                         p0, p1, good_new, good_old = recalculate_new_features(framequeue, feature_params, lk_params, frame_gray, p0, p1, good_new, good_old, old_gray)
-
-                        # if iterations > 1:
-                        #     old_frame = frame.copy()
-                        #     framequeue.append(frame.copy())
-                        #     cv2.imshow(VIDEO_WINDOW,frame)
-                        #     p0 = good_new.reshape(-1,1,2)
-                        #     cv2.waitKey(25)
-                        #     #skip to next frame
-                        #     print 'recreated features... skipping to next frame - %s' % dt.datetime.now()
-                        #     continue
                     except Exception as e:
                         print e
+                        varianceLRBuffer.append(np.nan)
+                        movementLRBuffer.append(np.nan)
+                        varianceUDBuffer.append(np.nan)
+                        movementUDBuffer.append(np.nan)
+                        old_gray = frame_gray.copy()
+                        old_frame = frame.copy()
+                        framequeue.append(old_frame)
+                        cv2.putText(vectorField, 'recalculating', (0,100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,225,0))
+                        writer.write(vectorField)
+                        update_windows(frame, vectorField, plot)
+                        continue
 
-                histogramLeftRight = np.zeros(50, dtype = np.float32)
-                histogramUpDown    = np.zeros(50, dtype = np.float32)
+                magnitudesLeftRight = []
+                magnitudesUpDown    = []
+
+                #store vector magnitudes
+                #magnitudes = []
 
                 good_old = p0[st==1]
 
@@ -276,26 +321,49 @@ try:
                     cv2.circle(vectorField, (a,b), 1, (0, 0, 255), -1)
 
                     #add data to calculate variance
-                    mag = np.sqrt((a-c) ** 2 + (b-d) ** 2)
-                    magnitudes.append(mag)
+                    #mag = np.sqrt((a-c) ** 2 + (b-d) ** 2)
+                    #magnitudes.append(mag)
 
                     #calculate direction of feature movement
-                    histogram[i] = a - c
+                    magnitudesLeftRight.append(a - c)
+                    magnitudesUpDown.append(b -d)
 
-                width = 300
-                for index, eachVariance in enumerate(varianceBuffer):
-                    if not eachVariance is np.nan:
-                        cv2.circle(plot, (index, int(eachVariance)), 1, (50,50,255), -1)
-
-                variance = np.var(magnitudes) / iterations
-                movement = np.mean(histogram) / iterations
-
-                draw_text_overlay(vectorField, varianceBuffer, movementBuffer, variance, movement, histogram)
+                #variance = np.var(magnitudes) / iterations
+                movementLeftRight = np.mean(magnitudesLeftRight) / FRAME_WIDTH / iterations
+                movementUpDown    = np.mean(magnitudesUpDown) / FRAME_HEIGHT / iterations
+                varianceLeftRight = np.var(magnitudesLeftRight) / FRAME_WIDTH / iterations
+                varianceUpDown    = np.var(magnitudesUpDown) / FRAME_WIDTH / iterations
 
                 # append values to buffers
                 for i in range(iterations):
-                    varianceBuffer.append(variance)
-                    movementBuffer.append(movement)
+                    if i == 0:
+                        pass
+                    else:
+                        varianceLRBuffer.pop()
+                        movementLRBuffer.pop()
+                        varianceUDBuffer.pop()
+                        movementUDBuffer.pop()
+                    varianceLRBuffer.append(varianceLeftRight)
+                    movementLRBuffer.append(movementLeftRight)
+                    varianceUDBuffer.append(varianceUpDown)
+                    movementUDBuffer.append(movementUpDown)
+
+                movementLeftRight_mean = np.nanmean(movementLRBuffer)
+                movementUpDown_mean    = np.nanmean(movementUDBuffer)
+                varianceLeftRight_mean = np.nanmean(varianceLRBuffer)
+                varianceUpDown_mean    = np.nanmean(varianceUDBuffer)
+
+                draw_text_overlay(vectorField,
+                                  varianceLeftRight_mean, varianceUpDown_mean, movementLeftRight_mean, movementUpDown_mean,
+                                  varianceLeftRight, movementLeftRight, varianceUpDown, movementUpDown)
+
+                for index, eachVariance in enumerate(varianceLRBuffer):
+                    if not eachVariance is np.nan:
+                        cv2.circle(plot, (index, int(eachVariance)), 1, (50,50,255), -1)
+
+                for index, eachVariance in enumerate(varianceUDBuffer):
+                    if not eachVariance is np.nan:
+                        cv2.circle(plot, (index, int(eachVariance)), 1, (50,255,50), -1)
 
                 # write frame to output
                 writer.write(vectorField)
